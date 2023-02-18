1adf4ca063a6629567d6a01cd2058286d0b98bf4
MVP-365 <mvp.magongo@gmail.com>
2023-02-17 18:35:17 -0800

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int intType;
+	float floatType;
+	double doubleType;
+	char charType;
+
+	//sizeof evaluates the size of a variable
+	printf("size of int: %zu bytes\n", sizeof(intTtype));
+	printf("size of float: %zu bytes\n", sizeof(floatType));
+	printf("size of double: %zu bytes\n", sizeof(doubleType));
+	printf("size of char: %zu byte\n", sizeof(charType));
+
+	return (0);
+}
