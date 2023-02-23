7aae62c3b391936cf03eec935f6d560a01289727
MVP-365 <mvp.magongo@gmail.com>
2023-02-22 17:25:08 -0800

    10-add.c

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -2,9 +2,9 @@
 
 /**
  * add - adds two intergers and returns the result
- * @a: int to be added to b
- * @b: int to be added to a
- * Return: sum of a and b
+ * @num1: first interger to be added
+ * @num2: second interger to be added
+ * Return: sum of the addition
  */
 
 int add(int num1, int num2)
