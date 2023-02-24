9c39b52ac1e5761c833296a7c40663d59fcc3089
MVP-365 <mvp.magongo@gmail.com>
2023-02-23 19:52:07 -0800

    more_functions_nested_loops

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * mul - multiplies two intergers
+ * @a: first interger
+ * @b: second interger
+ * Return: sum of a and b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
