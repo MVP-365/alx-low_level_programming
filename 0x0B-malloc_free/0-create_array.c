ddb9cfd795d94e6e4a9a54d2b944c3f841b47e4b
MVP-365 <mvp.magongo@gmail.com>
2023-03-14 20:00:32 -0700

    0x0B-malloc_free

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -6,20 +6,23 @@
  * and initializes it with a specific char.
  * @size: the size of the array
  * @c: initial value
- * Return: a pointer to the array, or Null if it fails
+ *
+ * Return: a pointer to the array, or NULL if it fails
  */
-
 char *create_array(unsigned int size, char c)
 {
 	char *array;
 	unsigned int i;
-	Null = 0;
 
-	array = malloc(sizeof(char) * size);
-	if (size == 0 || array == 0)
-		return (Null);
+	if (size == 0)
+		return (NULL);
+
+	array = (char *)malloc(sizeof(char) * size);
+	if (array == NULL)
+		return (NULL);
 
 	for (i = 0; i < size; i++)
 		array[i] = c;
+
 	return (array);
 }
